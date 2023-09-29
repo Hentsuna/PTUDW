dfd5915f89994387061b104a129fefe29bf7ae44
Hentsuna <damtrongj095@gmail.com>
2023-09-29 21:02:53 +0700

    Update ProjectDay1

--- ProjectDay1/ProjectDay1/Controllers/SiteController.cs
+++ ProjectDay1/ProjectDay1/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace ProjectDay1.Controllers
 {
@@ -11,6 +12,9 @@ namespace ProjectDay1.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();
+            int count = db.Products.Count();
+            ViewBag.choi = count;
             return View();
         }
     }
