51c129584956e3bf990c528743ed7bd66f49a7ce
Hentsuna <damtrongj095@gmail.com>
2023-10-17 15:54:04 +0700

    update

--- ProjectDay1/MyClass/Model/Categories.cs
+++ ProjectDay1/MyClass/Model/Categories.cs
@@ -14,29 +14,47 @@ namespace MyClass.Model
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="tên loại SP không để trống")]
+        [Display(Name="Tên loại SP")]
         public string Name { get; set; }
 
+        [Display(Name="Tên rút gọn")]
         public string Slug { get; set; }
 
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
 
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
-        [Required]
+        [Display(Name = "Mô tả")]
+
+        [Required(ErrorMessage = "Mô tả không để trống")]
         public string MetaDesc { get; set; }
 
-        [Required]
+        [Display(Name = "Từ khoá")]
+
+        [Required(ErrorMessage = "Từ khoá không để trống")]
         public string MetaKey { get; set; }
 
+        [Display(Name = "Tạo bởi")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
         public int CreatedBy { get; set; }
 
+        [Display(Name = "Ngày tạo")]
+        [Required(ErrorMessage = "Ngày tạo không để trống")]
         public DateTime CreatedAt { get; set; }
 
+        [Display(Name = "Cập nhật bởi")]
+        [Required(ErrorMessage = "Người cập nhật không để trống")]
         public int UpdateBy { get; set; }
 
+        [Display(Name = "Ngày cập nhật")]
+        [Required(ErrorMessage = "Ngày cập nhật không để trống")]
         public DateTime UpdateAt { get; set; }
 
+        [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái không để trống")]
         public int Status { get; set; }
     }
 }
---
+++ ProjectDay1/ProjectDay1/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.DAO;
+using MyClass.Model;
+using UDW.Library;
+
+namespace ProjectDay1.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesDAO categoriesDAO = new CategoriesDAO();
+
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View(categoriesDAO.getList("Index"));
+        }
+
+        // GET: Admin/Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
+            return View();
+        }
+
+        // POST: Admin/Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                //Xử lí tự động
+                categories.CreatedAt = DateTime.Now;
+                categories.UpdateAt = DateTime.Now;
+                if(categories.ParentId == null)
+                {
+                    categories.ParentId = 0;
+                }
+                if(categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;
+                }
+                categories.Slug = XString.Str_Slug(categories.Name);
+
+                //Chèn thêm dòng cho database 
+                categoriesDAO.Insert(categories);
+                return RedirectToAction("Index");
+            }
+
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDAO.Update(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categories categories = categoriesDAO.getRow(id);
+            categoriesDAO.Delete(categories);
+            return RedirectToAction("Index");
+        }
+
+        //STATUS
+        //GET: Admin/Category/Status/5
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                //thoong bao that bai
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
+            }
+            //Truy vấn id
+            Categories categories = categoriesDAO.getRow(id);
+            if(categories == null)
+            {
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
+            }    
+            //chuyển đổi trạng thái status 1<->2
+            categories.Status = (categories.Status == 1) ? 2 : 1;
+
+            //Cập nhật UpdateAt
+            categories.UpdateAt = DateTime.Now;
+
+            //Cập nhật lại data
+            categoriesDAO.Update(categories);
+
+            //Thông báo cập nhật trạng thái thành công
+            TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+            return RedirectToAction("Index");
+        }
+    }
+}
